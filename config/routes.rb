22accbe24950ef33c79951d990c410a96229e78e
jose <josewgomes0800@gmail.com>
2024-04-02 15:43:27 -0300

    Ativando-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
